e884622b33db4de0c7fa32e962316dd38a291d7e
Jay Termulo <jetermulo@gmail.com>
2024-02-25 00:25:15 +0800

    debug server error

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -9,7 +9,7 @@ const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL  || "http://localho
 export const useRegisterUser = () => {
   const registerUser = async (data: RegisterUser) => {
     try {
-      const response = await axios.post(`${BACKEND_URL}/api/users/register`, data)
+      const response = await axios.post(`${BACKEND_URL}api/users/register`, data)
       return response.data
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -37,7 +37,7 @@ export const useRegisterUser = () => {
 export const useLoginUser = () => {
   const loginUser = async (data: LoginUser) => {
     try {
-      const response = await axios.post(`${BACKEND_URL}/api/users/login`, data)
+      const response = await axios.post(`${BACKEND_URL}api/users/login`, data)
       if(response.data.success) {
         toast.success("Login Successful...")
       }
@@ -68,7 +68,7 @@ export const useLoginUser = () => {
 export const useLogoutUser = () => {
 
   const logoutUser = async () => {
-    await axios.get(`${BACKEND_URL}/api/users/logout`)
+    await axios.get(`${BACKEND_URL}api/users/logout`)
     return true
   }
   return useQuery({
@@ -82,7 +82,7 @@ export const useLogoutUser = () => {
 export const useForgotPassword = () => {
   const forgotPassword = async (data: ForgotPassword) => {
     try {
-      const response = await axios.post(`${BACKEND_URL}/api/users/forgotpassword`, data)
+      const response = await axios.post(`${BACKEND_URL}api/users/forgotpassword`, data)
       if(response.data.success) {
         toast.success(response.data.message)
       }
@@ -112,7 +112,7 @@ export const useForgotPassword = () => {
 export const useResetPassword = () => {
   const resetPassword = async ({ data, resetToken }:{data: ResetPassword, resetToken: string}) => {
     try {
-      const response = await axios.put(`${BACKEND_URL}/api/users/resetpassword/${resetToken}`, data)
+      const response = await axios.put(`${BACKEND_URL}api/users/resetpassword/${resetToken}`, data)
       return response.data
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -140,7 +140,7 @@ export const useResetPassword = () => {
 export const useGetLoginStatus = () => {
   const getLoginStatus = async () => {
     try {
-      const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`)
+      const response = await axios.get(`${BACKEND_URL}api/users/loggedin`)
       console.log("Backend URL:", BACKEND_URL);
       return response.data || {}
     } catch (error) {
